c9f1c19a1a4e7de8c69662396ba5330dad69b656
tsubasa.hidaka <hidaka@hyoubo.com>
2025-02-23 07:44:25 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,19 @@
+@if (Auth::check())
+    {{-- 日報登録ページへのリンク --}}
+    <li><a class="link link-hover" href="#">日報登録</a></li>
+    {{-- ログインユーザー情報ページへのリンク --}}
+    <li><a class="link link-hover" href="#">ユーザー情報</a></li>
+    {{-- ユーザー一覧ページへのリンク --}}
+    <li><a class="link link-hover" href="#">社員一覧</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ユーザー登録へのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ユーザー登録(未)</a></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザー登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">Login</a></li>
+@endif
